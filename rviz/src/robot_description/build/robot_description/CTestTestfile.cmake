7d0928eb23bdf6d8fe03ac17f27efe0093acd288
Justin <justin@justinhelland.com>
2025-03-13 16:37:40 -0700

    removed unnecessary sim files

--- rviz/src/robot_description/build/robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/j/ros2_ws/src/robot_description
-# Build directory: /home/j/ros2_ws/src/robot_description/build/robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
